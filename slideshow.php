5915bdf9bb469b54ee6d4d038d5f92163f0a43c1
Tim Thorp <tim@newenglandwebs.com>
2012-05-04 12:29:23 -0400

    HOLLA!

---
+++ slideshow.php
@@ -0,0 +1,31 @@
+<?php
+function gallery(){
+	//in future versions, add the directory to the arguments for the function
+	$directory = 'img/gallery';
+	/* step one:  read directory, make array of images */
+	if ($handle = opendir($directory)) {  
+		while (false !== ($file = readdir($handle)))  
+		{  
+			if ($file != '.' && $file != '..')  
+			{  
+				$files[] = $file;  
+			}  
+		}  
+		closedir($handle);  
+	}  
+	/* step two: loop through, format gallery */  
+	if(count($files))  
+	{
+		sort($files);
+		$count=0;
+		foreach($files as $file)  
+		{  
+			echo "<a href='$directory/$file'><img src='$directory/$file' class='product' /></a>";
+		}  
+	}  
+	else  
+	{  
+		echo '<p>There are no images in this gallery.</p>';  
+	}
+}
+?>
