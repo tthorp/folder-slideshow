7548bc37b71373892e1fed0ba32636309de1e840
Tim Thorp <tim@newenglandwebs.com>
2012-05-08 10:25:49 -0400

    added flexibilty to output and cleaned up indentation

--- slideshow.php
+++ slideshow.php
@@ -1,29 +1,32 @@
 <?php
-function gallery($directory){
+function gallery($directory,$display_type){
 	/* step one:  read directory, make array of images */
-	if ($handle = opendir($directory)) {  
-		while (false !== ($file = readdir($handle)))  
-		{  
-			if ($file != '.' && $file != '..')  
-			{  
-				$files[] = $file;  
-			}  
-		}  
-		closedir($handle);  
-	}  
-	/* step two: loop through, format gallery */  
-	if(count($files))  
-	{
+	if ($handle = opendir($directory)) {
+		while (false !== ($file = readdir($handle))){
+			if ($file != '.' && $file != '..'){
+				$files[] = $file;
+			}
+		}
+		closedir($handle);
+	}
+	/* step two: loop through, format gallery */
+	if(count($files)){
 		sort($files);
-		$count=0;
-		foreach($files as $file)  
-		{  
-			echo "<a href='$directory/$file'><img src='$directory/$file' class='product' /></a>";
-		}  
-	}  
-	else  
-	{  
-		echo '<p>There are no images in this gallery.</p>';  
+		foreach($files as $file){
+			switch($display_type){
+				case 'thumbnails':
+					echo "<a href='$directory/$file'><img src='$directory/$file' class='product' /></a>";
+					break;
+				case 'slideshow':
+					echo "<div class='slide'><img src='$directory/$file' class='product' /></a></div>";
+					break;
+				case 'list':
+					echo "<li><img src='$directory/$file' class='product' /></a></li>";
+					break;
+			}
+		}
+	}else{
+		echo '<p>There are no images in this gallery.</p>';
 	}
 }
 ?>
