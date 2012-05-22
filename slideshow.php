c34000f1a02948bf3babd2d474d15df807a9ff8c
Tim Thorp <tim@newenglandwebs.com>
2012-05-22 10:12:43 -0400

    set default show and added alt tags

--- slideshow.php
+++ slideshow.php
@@ -1,5 +1,5 @@
 <?php
-function gallery($directory,$display_type){
+function gallery($directory,$display_type = 'slideshow'){
 	/* step one:  read directory, make array of images */
 	if ($handle = opendir($directory)) {
 		while (false !== ($file = readdir($handle))){
@@ -13,15 +13,23 @@ function gallery($directory,$display_type){
 	if(count($files)){
 		sort($files);
 		foreach($files as $file){
+			$start = 0;
+			$end = strpos($file, '.jpg');
+			$caption = substr($file, $start, $end);
+			$caption = str_replace('-',' ',$caption);
+			$caption = ucfirst(str_replace('_',' ',$caption));
 			switch($display_type){
 				case 'thumbnails':
-					echo "<a href='$directory/$file'><img src='$directory/$file' class='product' /></a>";
+					echo "<a href='$directory/$file'><img src='$directory/$file' class='product' alt='$caption' /></a>";
 					break;
 				case 'slideshow':
-					echo "<div class='slide'><img src='$directory/$file' class='product' /></a></div>";
+					echo "<div class='slide'><img src='$directory/$file' class='product' alt='$caption' /></a></div>";
+					break;
+				case 'captioned-slideshow':
+					echo "<div class='slide'><img src='$directory/$file' class='product' alt='$caption' /></a><h2 class='caption'>$caption</h2></div>";
 					break;
 				case 'list':
-					echo "<li><img src='$directory/$file' class='product' /></a></li>";
+					echo "<li><img src='$directory/$file' class='product' alt='$caption' /></a></li>";
 					break;
 			}
 		}
